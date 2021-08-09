de781b024319054b31bedb0f7e650ae997f1ad85
OmarMohamed256 <omarmohamed2342@outlook.com>
2021-08-09 22:53:24 +0200

    added Tasbeeh screen configured main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_application/TasbeehScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        TasbeehScreen.routeName:(context)=>TasbeehScreen(),
+      },
+      initialRoute: TasbeehScreen.routeName,
 
     );
   }
