96e9686b33a411fcc3e9b41024c8157a0ce19a6f
Eyadzz <eyadyoussef1336@gmail.com>
2021-08-11 18:39:20 +0200

    bottom nav ui update

--- lib/CustomBottomBar.dart
+++ lib/CustomBottomBar.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:quran_application/QuranScreen.dart';
 import 'package:quran_application/TasbeehScreen.dart';
-import 'package:quran_application/HadethScreen.dart';
 class CustomBottomBar extends StatefulWidget {
   const CustomBottomBar ({Key? key, this.mypage}) : super(key: key);
   static const routeName = "home";
-  final iconSize = 100.0, iconColor = Colors.white;
+  final iconSize = 30.0;
 
   final quranPageName = QuranScreen.routeName;
-  final hadisPageName = HadethScreen.routeName;
+  final hasdisPageName = QuranScreen.routeName;
   final sebhaPageName = TasbeehScreen.routeName;
   final radioPageName = QuranScreen.routeName;
 
@@ -19,6 +18,16 @@ class CustomBottomBar extends StatefulWidget {
 
 class _CustomBottomBarState extends State<CustomBottomBar> {
 
+  var mainColor = Colors.white, activeColor = Colors.black;
+  var currentIndex = 0;
+
+  changeColor(index, currentIndex){
+    if(index == currentIndex){
+      return activeColor;
+    }else{
+      return mainColor;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,15 +40,17 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-
               IconButton(
                 onPressed: (){
                   //Navigator.pushReplacementNamed(context, widget.quranPageName);
                   widget.mypage.jumpToPage(0);
+                  setState(() {
+                    currentIndex = 0;
+                  });
                 },
                 icon: ImageIcon(
                   AssetImage("assets/images/moshaf_blue@3x.png"),
-                  color: widget.iconColor,
+                  color: changeColor(0, currentIndex),
                   size: widget.iconSize,
                 ),
               ),
@@ -47,10 +58,13 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
                 onPressed: (){
                   //Navigator.pushReplacementNamed(context, widget.radioPageName);
                   widget.mypage.jumpToPage(1);
+                  setState(() {
+                    currentIndex = 1;
+                  });
                 },
                 icon: ImageIcon(
                   AssetImage("assets/images/radio_blue@3x.png"),
-                  color: widget.iconColor,
+                  color: changeColor(1, currentIndex),
                   size: widget.iconSize,
                 ),
               ),
@@ -58,21 +72,29 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
                 onPressed: (){
                   //Navigator.pushReplacementNamed(context, widget.sebhaPageName);
                   widget.mypage.jumpToPage(2);
+                  setState(() {
+                    currentIndex = 2;
+                  });
                 },
                 icon: ImageIcon(
                   AssetImage("assets/images/sebha_blue@3x.png"),
-                  color: widget.iconColor,
+                  //color: widget.iconColor,
+                  color: changeColor(2, currentIndex),
                   size: widget.iconSize,
                 ),
               ),
               IconButton(
                 onPressed: (){
-                  //Navigator.pushReplacementNamed(context, widget.hadisPageName);
+                  //Navigator.pushReplacementNamed(context, widget.hasdisPageName);
                   widget.mypage.jumpToPage(3);
+                  setState(() {
+                    currentIndex = 3;
+                  });
                 },
                 icon: ImageIcon(
                   AssetImage("assets/images/moshaf@3x.png"),
-                  color: widget.iconColor,
+                  //color: widget.iconColor,
+                  color: changeColor(3, currentIndex),
                   size: widget.iconSize,
                 ),
               ),
