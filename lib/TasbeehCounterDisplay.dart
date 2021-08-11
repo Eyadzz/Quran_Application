678e82c599b37899e4e2f8b7fdb88a66b191073f
OmarMohamed256 <omarmohamed2342@outlook.com>
2021-08-11 18:17:46 +0200

    fixed Tasbeeh Screen alignment issue

--- lib/TasbeehCounterDisplay.dart
+++ lib/TasbeehCounterDisplay.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TasbeehCounterDisplay extends StatefulWidget {
-  TasbeehCounterDisplay({Key? key}) : super(key: key);
+  TasbeehCounterDisplay({Key? key, required this.imageToRotate}) : super(key: key);
+  final Image imageToRotate;
 
   @override
   _TasbeehCounterDisplayState createState() => _TasbeehCounterDisplayState();
@@ -11,6 +12,20 @@ class TasbeehCounterDisplay extends StatefulWidget {
 class _TasbeehCounterDisplayState extends State<TasbeehCounterDisplay> {
   int _tasbeehCounter = 0;
   String tasbeehButtonLabel = 'سبحان الله';
+  late Image imageToRotate;
+  bool rotate = true;
+
+  @override
+  void initState() {
+    super.initState();
+    imageToRotate = widget.imageToRotate;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    precacheImage(imageToRotate.image, context);
+  }
 
   void _changeTasbeehButtonDisplay() {
     if (tasbeehButtonLabel == 'سبحان الله') {
@@ -38,7 +53,32 @@ class _TasbeehCounterDisplayState extends State<TasbeehCounterDisplay> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
+
+        GestureDetector(
+          child: Transform(
+            transform: Matrix4.rotationX((rotate ? 0 : -2) * 3.14159 / 2),
+            alignment: Alignment.center,
+            child: Container(
+              alignment: Alignment.center,
+              child: imageToRotate,
+            ),
+          ),
+          onTap: () {
+            setState(() {
+              rotate = !rotate;
+              _incrementTasbeehCounter();
+            });
+          },
+        ),
+        SizedBox(
+          height: 10,
+        ),
+
         Text('عدد التسبيحات', textScaleFactor: 2.5),
+        SizedBox(
+          height: 20,
+        ),
+
         ElevatedButton(
             onPressed: null,
             child: Text(
@@ -52,18 +92,22 @@ class _TasbeehCounterDisplayState extends State<TasbeehCounterDisplay> {
                 minimumSize: MaterialStateProperty.all(Size(60,80)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                     RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                )))),
+                      borderRadius: BorderRadius.circular(25.0),
+                    )))),
+        SizedBox(
+          height: 10,
+        ),
+
         ElevatedButton(
-            onPressed: _incrementTasbeehCounter,
+            onPressed: null,
             child: Text(tasbeehButtonLabel, textScaleFactor: 1.6),
             style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color.fromRGBO(183, 147, 95, 1)),
+                backgroundColor: MaterialStateProperty.all(Colors.green),
                 minimumSize: MaterialStateProperty.all(Size(150,40)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                     RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(25.0),
-                        side: BorderSide(color: Color.fromRGBO(183, 147, 95, 1))))))
+                        side: BorderSide(color: Colors.green)))))
       ],
     );
   }
--- lib/TasbeehScreen.dart
+++ lib/TasbeehScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'TasbeehCounterDisplay.dart';
-import 'ImageRotation.dart';
 
 class TasbeehScreen extends StatelessWidget {
   static const routeName = 'tasbeeh';
@@ -9,49 +8,48 @@ class TasbeehScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(
-      children: <Widget>[
-        Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("assets/images/bg3.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 80,),
-              Expanded(
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 15,
-                      left: 180,
-                      child: Container(
-                        child: Image(
-                          image: AssetImage('assets/images/headofseb7a.png'),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 90,
-                      left: 80,
-                      child: Container(
-                        child: ImageRotation(
-                            imageToRotate: Image(
-                          image: AssetImage('assets/images/bodyofseb7a.png'),
-                        )),
-                      ),
-                    ),
-                  ],
+          children: <Widget>[
+            Container(
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: new AssetImage("assets/images/bg3.png"),
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 20,),
-              Expanded(child: TasbeehCounterDisplay()),
-            ])
-      ],
-    ));
+            ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 100,
+                          left: 160,
+                          child: Container(
+                            child: Image(
+                              image: AssetImage('assets/images/headofseb7a.png'),
+                            ),
+                          ),
+                        ),
+
+                        Positioned(
+                          top: 175,
+                          left: 80,
+                          child: Container(
+                            child: TasbeehCounterDisplay(
+                                imageToRotate: Image(
+                                  image: AssetImage('assets/images/bodyofseb7a.png'),
+                                )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ])
+          ],
+        ));
   }
 }
