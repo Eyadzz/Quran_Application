924c11476234b59442cee1edaa23bb3af2a2dcbd
Eyadzz <eyadyoussef1336@gmail.com>
2021-08-18 19:25:11 +0200

    naming changes

--- lib/QuranScreen.dart
+++ lib/QuranScreen.dart
@@ -5,6 +5,7 @@ import 'package:quran_application/ReadQuran.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'AppConfigProvider.dart';
+import 'ReadQuran.dart';
 
 class QuranScreen extends StatefulWidget {
 
@@ -16,23 +17,25 @@ class QuranScreen extends StatefulWidget {
 }
 
 class _QuranScreenState extends State<QuranScreen> {
+
   late AppConfigProvider provider;
-  var surasNames=[];
-  var surasNums=[];
+  var surahsNames=[];
+  var surahsVerses=[];
   var colorTheme = Color.fromRGBO(183, 147, 95, 1);
   var colorDarkTheme = Color.fromRGBO(252,196,64,1);
+
   @override
   void  initState(){
-    getContent();
+    getSurahContent();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     provider = Provider.of<AppConfigProvider>(context);
-    var arborder = BoxDecoration(border: Border(left: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),bottom: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),top: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,)));
-    var enborder = BoxDecoration(border: Border(right: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),bottom: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),top: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,)));
-    var currentBorder = (provider.currentLocale == 'en')? enborder: arborder;
+    var arHeaderBorders = BoxDecoration(border: Border(left: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),bottom: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),top: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,)));
+    var enHeaderBorders = BoxDecoration(border: Border(right: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),bottom: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),top: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,)));
+    var headerBorders = (provider.currentLocale == 'en')? enHeaderBorders: arHeaderBorders;
 
     return Scaffold(
         body: Stack(
@@ -63,19 +66,17 @@ class _QuranScreenState extends State<QuranScreen> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          header(AppLocalizations.of(context)!.surahName, currentBorder),
-                          header(AppLocalizations.of(context)!.numOfVerses,  BoxDecoration(border: Border(bottom: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),top: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,)))),
-
+                          header(AppLocalizations.of(context)!.surahName, headerBorders),
+                          header(AppLocalizations.of(context)!.numOfVerses, BoxDecoration(border: Border(bottom: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,),top: BorderSide(color: provider.isDarkTheme()? colorDarkTheme:colorTheme,width: 3,)))),
                         ],
                       ),
                       Expanded(
                         child: ListView.builder(
-
                           padding: EdgeInsets.only(top: 0.0),
                           scrollDirection: Axis.vertical,
                           shrinkWrap: true,
-                          itemBuilder: (context, index) => buildContent(surasNames.elementAt(index), surasNums.elementAt(index), index+1),
-                          itemCount: surasNames.length,
+                          itemBuilder: (context, index) => buildListOfSurahs(surahsNames.elementAt(index), surahsVerses.elementAt(index), index+1),
+                          itemCount: surahsNames.length,
                         ),
                       ),
                     ],
@@ -88,38 +89,39 @@ class _QuranScreenState extends State<QuranScreen> {
     );
   }
 
-  Future<String> loadData(String path) async {
+  Future<String> loadDataFromFile(String path) async {
     return await rootBundle.loadString(path);
   }
 
-  getContent()
+  getSurahContent()
   async {
-    String data = await loadData('assets/content/sura_names.txt');
-    surasNames=data.split("\n");
-    data = await loadData('assets/content/suras_nums.txt');
-    surasNums=data.split("\n");
+    String data = await loadDataFromFile('assets/content/sura_names.txt');
+    surahsNames=data.split("\n");
+    data = await loadDataFromFile('assets/content/suras_nums.txt');
+    surahsVerses=data.split("\n");
     setState(() {});
   }
 
-  Widget buildContent(String name, String number, int index)
+  Widget buildListOfSurahs(String surahName, String surahVerses, int index)
   {
-    var contentArBorder = BoxDecoration(border: Border(left: BorderSide(color: provider.isDarkTheme()? colorDarkTheme: colorTheme,width: 3,)));
-    var contentEnBorder = BoxDecoration(border: Border(right: BorderSide(color: provider.isDarkTheme()? colorDarkTheme: colorTheme,width: 3,)));
-    var contentCurrentBorder = (provider.currentLocale == 'en')? contentEnBorder: contentArBorder;
+    var arContentBorders = BoxDecoration(border: Border(left: BorderSide(color: provider.isDarkTheme()? colorDarkTheme: colorTheme,width: 3,)));
+    var enContentBorders = BoxDecoration(border: Border(right: BorderSide(color: provider.isDarkTheme()? colorDarkTheme: colorTheme,width: 3,)));
+    var borders = (provider.currentLocale == 'en')? enContentBorders: arContentBorders;
     return Row(
       children: [
         Expanded(
           child: Container(
-            decoration: contentCurrentBorder,
+            decoration: borders,
             child: TextButton(
               onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>ReadQuran(name,index,true)));
+                //Navigator.pushNamed(context,ReadQuran.routeName,arguments: ReadQuran(surahName,index,true));
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>ReadQuran(surahName,index,true)));
               },
               style: TextButton.styleFrom(
                 padding: EdgeInsets.zero,
               ),
               child: Text(
-                name,
+                surahName,
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
@@ -128,7 +130,7 @@ class _QuranScreenState extends State<QuranScreen> {
         Expanded(
           child: Center(
             child:  Text(
-              number,
+              surahVerses,
               style: Theme.of(context).textTheme.bodyText1,
             ),
           ),
@@ -137,13 +139,13 @@ class _QuranScreenState extends State<QuranScreen> {
     );
   }
 
-  Widget header(String text, var containerBorders)
+  Widget header(String title, var containerBorders)
   {
     return Expanded(
       child: Container(
         decoration: containerBorders,
         child: Center(
-          child: Text(text,
+          child: Text(title,
             style: Theme.of(context).textTheme.headline1
           ),
         ),
